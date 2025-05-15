1a4072754c0771147a0064229b7af167dbb0ed86
ats1999 <rkt82224@gmail.com>
2025-05-16 03:52:08 +0530

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import authRoutes from './routes/auth.route';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+
+app.use(errorHandler);
 
 app.get('/', (_req, res) => {
     res.send('Hello TypeScript with Express!');
