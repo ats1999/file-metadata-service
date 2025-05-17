5c64562a18afb67aad0d6c4c2aefcd442ebb12fb
ats1999 <rkt82224@gmail.com>
2025-05-17 15:25:11 +0530

    added rate limit

--- api-server/src/routes/file.route.ts
+++ api-server/src/routes/file.route.ts
@@ -1,12 +1,25 @@
 import { Router } from "express";
+import expressRateLimit from "express-rate-limit";
 import {
   uploadFile,
   getFileInfo,
   getFilesInfo,
 } from "../controllers/file.controller";
+import { getUploadRateLimitOptions } from "../utils";
+
+const updateRateLimitOptions = getUploadRateLimitOptions();
+
+const userRateLimiter = expressRateLimit({
+  windowMs: updateRateLimitOptions.windowMs as number, // 1 day
+  max: updateRateLimitOptions.max as number,
+  message: { error: "Too many requests, try again later." },
+  keyGenerator: (req, _) => {
+    return String(req.tokenPayload?.userId);
+  },
+});
 
 const router = Router();
-
+router.use("/upload", userRateLimiter);
 router.post("/upload", uploadFile);
 router.get("/:fileId", getFileInfo);
 router.get("/", getFilesInfo);
